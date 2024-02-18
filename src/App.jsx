9da493b78c859ba8336b6982b049e0702ff819e1
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2024-02-18 12:47:45 +0530

    Started working on LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import Landing from "./Pages/Landing/Landing";
+
 const App = () => {
-  return <main>this is home page</main>;
+  // return <main>this is home page</main>;
+  return <Landing />;
 };
 
 export default App;
